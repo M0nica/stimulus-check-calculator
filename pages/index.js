22975c5e5a6cc36fec57d12b592aed6a9089e839
Monica Powell <monica@aboutmonica.com>
2020-04-02 19:30:17 -0400

    misc. refactoring of the form

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import Head from "next/head";
 import React, { useState } from "react";
 import styles from "./index.module.css";
 
+const SINGLE = "Single";
+const HEADOFHOUSE = "Head of Household";
+const MARRIED = "Married";
+
 function getStimulusAmount(incomeLimit, baseStimulusAmount, income, children) {
   const ineligibleIncome = parseInt(income) - incomeLimit;
   const amountPerKid = 500 * children;
@@ -16,12 +20,12 @@ function getStimulusAmount(incomeLimit, baseStimulusAmount, income, children) {
 
 function calculateStimulus(income, filingStatus, children) {
   switch (filingStatus) {
-    case "Head of Household":
+    case HEADOFHOUSE:
       return getStimulusAmount(112500, 1200, income, children);
-    case "Single":
+    case SINGLE:
       return getStimulusAmount(75000, 1200, income, children);
       break;
-    case "Married":
+    case MARRIED:
       return getStimulusAmount(150000, 2400, income, children);
     default:
       return getStimulusAmount(75000, 1200, income, children);
@@ -46,34 +50,32 @@ function Home() {
         <title>Stimulus Check Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <h1>Stimulus Check Calculator</h1>
+        <h1>Stimulus Check Calculator 💸</h1>
+        <p>
+          Use this calculator to determine the estimated amount of your stimulus
+          check under the CARES Act.
+        </p>
         <form onSubmit={handleSubmit}>
-          <p>
-            This is an educational form based on figures from the{" "}
-            <a href="https://www.washingtonpost.com/context/coronavirus-aid-relief-and-economic-security-act-of-2020/28f5de5f-6eed-46c0-b629-798e219497c5/">
-              CARES Act
-            </a>{" "}
-            📝 .{" "}
-          </p>
-          <p>None of the information you input will be stored.</p>
           <label htmlFor="tax-year">Have you filed your 2019 taxes yet?</label>
           {[2019, 2018].map(year => (
             <button
               onClick={() => setTaxYear(year)}
               className={year == taxYear ? "selectedButton" : ""}
               key={year}
+              name="tax-year"
             >
               {year == 2019 ? "Yes" : "No"}
             </button>
           ))}
-          <p>What was your filing status in your {taxYear} taxes? </p>
-          {["Single", "Married", "Head of Household"].map(status => (
+          <label htmlFor="filing-status">
+            What was your filing status in your {taxYear} taxes?{" "}
+          </label>
+          {[SINGLE, MARRIED, HEADOFHOUSE].map(status => (
             <button
               onClick={() => setFilingStatus(status)}
               className={status == filingStatus ? "selectedButton" : ""}
-              name="tax-year"
+              name="filing-status"
               key={status}
             >
               {" "}
@@ -87,6 +89,8 @@ function Home() {
           ${" "}
           <input
             type="number"
+            inputMode="numeric"
+            pattern="[0-9]*"
             value={income}
             onChange={e => setIncome(e.target.value)}
             min={0}
@@ -99,6 +103,8 @@ function Home() {
           </label>
           <input
             type="number"
+            inputMode="numeric"
+            pattern="[0-9]*"
             value={children}
             onChange={e => setChildren(e.target.value)}
             min={0}
@@ -115,17 +121,34 @@ function Home() {
                 ? `Your stimulus amount is expected to be $${stimulusAmount}.`
                 : `You are not expected to receive a stimulus.`)}
           </p>
+          <br />
         </form>
       </main>
 
       <footer>
-        <a
-          href="https://www.github.com/m0nica"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Created By: Monica Powell
-        </a>
+        <p>
+          This form was created for educational purposes and based off of
+          figures from the{" "}
+          <a href="https://www.washingtonpost.com/context/coronavirus-aid-relief-and-economic-security-act-of-2020/28f5de5f-6eed-46c0-b629-798e219497c5/">
+            CARES Act
+          </a>{" "}
+          and this{" "}
+          <a href="https://www.washingtonpost.com/graphics/business/coronavirus-stimulus-check-calculator/">
+            Washington Post Article
+          </a>
+          .
+        </p>
+        <p>None of the information you input will be stored.</p>
+        <p>
+          Created By:{" "}
+          <a
+            href="https://www.github.com/m0nica"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Monica Powell
+          </a>
+        </p>
       </footer>
     </div>
   );
